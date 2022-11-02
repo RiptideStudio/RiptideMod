8fa381cdd5be7fb6368a267335fdaeca1cd36f79
RiptideStudio <59582654+RiptideStudio@users.noreply.github.com>
2022-11-02 00:09:50 -0700

    Add files via upload

---
+++ Projectiles/GelYoyoProjectile.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using RiptideMod.Projectiles;
+using RiptideMod;
+
+namespace RiptideMod.Projectiles
+{
+    public class GelYoyoProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("The Adhesive");
+            ProjectileID.Sets.YoyosLifeTimeMultiplier[Projectile.type] = 8;
+            ProjectileID.Sets.YoyosMaximumRange[Projectile.type] = 260;
+            ProjectileID.Sets.YoyosTopSpeed[Projectile.type] = 15;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.aiStyle = 99;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.penetrate = -1;
+        }
+    }
+}
