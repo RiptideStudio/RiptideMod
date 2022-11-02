b5bb3fa45831c4702f56ee51a19ba33d9816a8a0
RiptideStudio <59582654+RiptideStudio@users.noreply.github.com>
2022-11-02 00:09:00 -0700

    Add files via upload

---
+++ Items/GelYoyo.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using RiptideMod.Projectiles;
+using RiptideMod;
+
+namespace RiptideMod.Items
+{
+    public class GelYoyo : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("The Adhesive");
+            Tooltip.SetDefault("Has a sticky grip");
+            ItemID.Sets.Yoyo[Item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 14;
+            Item.DamageType = DamageClass.Generic;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 32;
+            Item.useAnimation = 32;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.knockBack = 2;
+            Item.value = 2000;
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.noUseGraphic = true;
+            Item.channel = true;
+            Item.shoot = ModContent.ProjectileType<GelYoyoProjectile>();
+            Item.shootSpeed = 6f;
+            Item.noMelee = true;
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            var lineToChange = tooltips.FirstOrDefault(x => x.Name == "Damage" && x.Mod == "Terraria");
+            if (lineToChange != null)
+            {
+                string[] split = lineToChange.Text.Split(' ');
+                lineToChange.Text = split.First() + " sticky " + split.Last();
+            }
+        }
+
+        public override void ModifyWeaponDamage(Player player, ref StatModifier damage)
+        {
+            damage += player.GetModPlayer<GlobalPlayer>().stickyDamage;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.WoodYoyo);
+            recipe.AddIngredient(ItemID.Gel, 12);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+
+    }
+}
