26d46a0425e41314880d3d4dbed0a499f2e67de1
RiptideStudio <59582654+RiptideStudio@users.noreply.github.com>
2023-07-19 10:52:18 -0700

    Add files via upload

---
+++ Tiles/GelBlock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RiptideMod.Tiles
+{
+    public class GelBlock : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Gel Block");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.useTime = 10;
+            Item.useAnimation = 10;
+            Item.autoReuse = true;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = 25;
+            Item.width = 8;
+            Item.height = 8;
+            Item.createTile = ModContent.TileType<GelBlockTile>();
+            Item.consumable = true;
+            Item.maxStack = 999;
+            Item.useStyle = ItemUseStyleID.Swing;
+        }
+    }
+}
---
+++ Tiles/GelBlockTile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RiptideMod.Tiles
+{
+    public class GelBlockTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = false;
+            Main.tileBouncy[Type] = true;
+
+            DustType = DustID.BubbleBurst_Blue;
+            ItemDrop = ModContent.ItemType<GelBlock>();
+        }
+    }
+}
